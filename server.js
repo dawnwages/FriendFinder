1280473fc55bd2dad478312048e2a89fb950c5e6
Dawn Wages <dawn.wages@gmail.com>
2018-03-11 22:46:29 -0400

    express and css

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
+
 
 
 var app = express();
@@ -14,11 +16,12 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 require("./routing/apiRoutes")(app);
-// require("./routing/htmlRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
